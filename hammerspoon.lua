188fc4d2fdee50ef967362226761d398d4e76e25
Luke <luke.jewers@gmail.com>
2024-05-26 21:42:18 +0100

    update keybindings

--- hammerspoon.lua
+++ hammerspoon.lua
@@ -1,11 +1,11 @@
+hs.hotkey.bind({"ctrl", "cmd"}, "j", function()
+    hs.application.open('Emacs.app')
+end)
+
 hs.hotkey.bind({"ctrl", "cmd"}, "k", function()
     hs.application.open('Kitty.app')
 end)
 
-hs.hotkey.bind({"ctrl", "cmd"}, "e", function()
-    hs.application.open('Emacs.app')
-end)
-
-hs.hotkey.bind({"ctrl", "cmd"}, "b", function()
+hs.hotkey.bind({"ctrl", "cmd"}, "l", function()
     hs.application.open('Google Chrome.app')
 end)
