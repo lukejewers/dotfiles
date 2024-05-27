559cb7c24fb3629278389e85f9ff11cd1467eed2
Luke <luke.jewers@gmail.com>
2024-05-27 21:20:24 +0100

    Update hammerspoon

--- hammerspoon.lua
+++ hammerspoon.lua
@@ -1,45 +1,39 @@
-local hyper = { "ctrl", "cmd" }
-local screen_states = { FULLSCREEN = 0, TWOPANE = 1}
-local screen_state = screen_states.FULLSCREEN
+local hyper         = { "ctrl", "cmd" }
+local screen_states = { FULLSCREEN = 0, TWOPANE = 1 }
+local screen_state  = screen_states.FULLSCREEN
+local emacs_app     = 'Emacs.app'
+local kitty_app     = 'Kitty.app'
+local chrome_app    = 'Google Chrome.app'
+
+hs.window.animationDuration = 0
+
+local function call_app_fullscreen (key, app)
+    hs.hotkey.bind(hyper, key, function()
+        hs.application.open(app)
+        if screen_state == screen_states.FULLSCREEN then
+           hs.window.focusedWindow():maximize()
+        end
+    end)
+end
 
 hs.hotkey.bind(hyper, "r", hs.reload)
 hs.hotkey.bind(hyper, "c", hs.toggleConsole)
 
-hs.window.animationDuration = 0
+call_app_fullscreen("j", emacs_app)
+call_app_fullscreen("k", kitty_app)
+call_app_fullscreen("l", chrome_app)
 
 hs.hotkey.bind(hyper, "z", function()
     if screen_state == screen_states.FULLSCREEN then
        screen_state = screen_states.TWOPANE
        -- Right side of screen
-       hs.application.open('Google Chrome.app')
+       hs.application.open(chrome_app)
        hs.window.focusedWindow():moveToUnit({0.5, 0, 0.5, 1})
        -- Left side of screen
-       hs.application.open('Emacs.app')
+       hs.application.open(emacs_app)
        hs.window.focusedWindow():moveToUnit({0, 0, 0.5, 1})
-       --
-    else
+    elseif screen_state == screen_states.TWOPANE then
        screen_state = screen_states.FULLSCREEN
        hs.window.focusedWindow():moveToUnit({0, 0, 1, 1})
-    end
-end)
-
-hs.hotkey.bind(hyper, "j", function()
-    hs.application.open('Emacs.app')
-    if screen_state == screen_state.FULLSCREEN then
-       hs.window.focusedWindow():maximize()
-    end
-end)
-
-hs.hotkey.bind(hyper, "k", function()
-    hs.application.open('Kitty.app')
-    if screen_state == screen_state.FULLSCREEN then
-       hs.window.focusedWindow():maximize()
-    end
-end)
-
-hs.hotkey.bind(hyper, "l", function()
-    hs.application.open('Google Chrome.app')
-    if screen_state == screen_state.FULLSCREEN then
-       hs.window.focusedWindow():maximize()
     end
 end)
