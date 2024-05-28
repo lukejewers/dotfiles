693b5a14bfc7f3a0903819a5645a02f20e2fed3a
Luke <luke.jewers@gmail.com>
2024-05-28 20:59:57 +0100

    Update hammerspoon

--- hammerspoon.lua
+++ hammerspoon.lua
@@ -16,6 +16,24 @@ local function call_app_fullscreen (key, app)
     end)
 end
 
+local function switch_layouts (key, screen_state_layout, app1, app2)
+    hs.hotkey.bind(hyper, key, function()
+        if screen_state_layout == screen_states.TWOPANE then
+           screen_state = screen_state_layout
+           -- Right side of screen
+           hs.application.open(app2)
+           hs.window.focusedWindow():moveToUnit({0.5, 0, 0.5, 1})
+           -- Left side of screen
+           hs.application.open(app1)
+           hs.window.focusedWindow():moveToUnit({0, 0, 0.5, 1})
+        elseif screen_state_layout == screen_states.FULLSCREEN then
+           screen_state = screen_state_layout
+           screen_state = screen_states.FULLSCREEN
+           hs.window.focusedWindow():moveToUnit({0, 0, 1, 1})
+        end
+    end)
+end
+
 hs.hotkey.bind(hyper, "r", hs.reload)
 hs.hotkey.bind(hyper, "c", hs.toggleConsole)
 
@@ -23,17 +41,7 @@ call_app_fullscreen("j", emacs_app)
 call_app_fullscreen("k", kitty_app)
 call_app_fullscreen("l", chrome_app)
 
-hs.hotkey.bind(hyper, "z", function()
-    if screen_state == screen_states.FULLSCREEN then
-       screen_state = screen_states.TWOPANE
-       -- Right side of screen
-       hs.application.open(chrome_app)
-       hs.window.focusedWindow():moveToUnit({0.5, 0, 0.5, 1})
-       -- Left side of screen
-       hs.application.open(emacs_app)
-       hs.window.focusedWindow():moveToUnit({0, 0, 0.5, 1})
-    elseif screen_state == screen_states.TWOPANE then
-       screen_state = screen_states.FULLSCREEN
-       hs.window.focusedWindow():moveToUnit({0, 0, 1, 1})
-    end
-end)
+switch_layouts("0", screen_states.FULLSCREEN)
+switch_layouts("1", screen_states.TWOPANE, emacs_app, kitty_app)
+switch_layouts("2", screen_states.TWOPANE, emacs_app, chrome_app)
+switch_layouts("3", screen_states.TWOPANE, kitty_app, chrome_app)
