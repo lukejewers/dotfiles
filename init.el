fb74ef75b65b845f74374d7a70a778825275354e
Luke <luke.jewers@gmail.com>
2025-06-26 21:29:23 +0100

    [emacs] shell

--- init.el
+++ init.el
@@ -551,5 +551,6 @@
       (funcall shell))))
 (global-set-key (kbd "C-z C-e") (lambda () (interactive) (toggle-shell "eshell-mode" 'eshell)))
 (global-set-key (kbd "C-z C-z") (lambda () (interactive) (toggle-shell "vterm-mode" 'vterm)))
+(global-set-key (kbd "C-z C-s") (lambda () (interactive) (toggle-shell "shell-mode" 'shell)))
 
 (provide 'init)
