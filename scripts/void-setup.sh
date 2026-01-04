e0ff7b368cb37e2f926e613d5af68465f888d55d
luke <luke.jewers@gmail.com>
2026-01-04 22:23:08 +0000

    [scripts/void-setup] add gsettings & more

--- scripts/void-setup.sh
+++ scripts/void-setup.sh
@@ -7,25 +7,24 @@ sudo xbps-install -Syu
 sudo xbps-install -y \
     linux-firmware-amd \
     mesa-dri mesa-vaapi mesa-vdpau vulkan-radeon libgbm \
-    tlp elogind dbus
+    tlp elogind dbus bluez
 
 sudo xbps-install -y \
-    sway waybar ghostty dmenu rofi \
-    wl-clipboard nautilus wget xtools git \
-    firefox emacs-pgtk \
-    font-iosevka nerd-fonts font-iosevka-nerd
+    gsettings-desktop-schemas sway waybar ghostty rofi \
+    nautilus firefox emacs-pgtk \
+    cmake libtool gdb git wget xtools wl-clipboard \
+    font-iosevka-nerd nerd-fonts font-logos
+
 
 sudo xbps-install -y \
     pipewire wireplumber pipewire-pulse alsa-pipewire \
-    pavucontrol sof-firmware
+    pavucontrol sof-firmware alsa-utils
 
 echo "Setting up dotfiles..."
 rm -rf ~/.dotfiles
 git clone https://github.com/lukejewers/dotfiles.git ~/.dotfiles
 
-mkdir -p ~/.config/sway
-mkdir -p ~/.config/waybar
-mkdir -p ~/.config/ghostty
+mkdir -p ~/.config/sway ~/.config/waybar ~/.config/ghostty ~/.emacs.d
 
 ln -sf ~/.dotfiles/.sway ~/.config/sway/config
 ln -sf ~/.dotfiles/.waybar.config ~/.config/waybar/config.jsonc
@@ -36,19 +35,43 @@ ln -sf ~/.dotfiles/.emacs ~/.emacs.d/init.el
 sudo ln -sf /etc/sv/dbus /var/service/
 sudo ln -sf /etc/sv/elogind /var/service/
 sudo ln -sf /etc/sv/tlp /var/service/
+sudo ln -sf /etc/sv/bluetoothd /var/service/
 
-sudo gpasswd -a $(whoami) _seatd
-sudo gpasswd -a $(whoami) video
-sudo gpasswd -a $(whoami) audio
+sudo gpasswd -a $USER video
+sudo gpasswd -a $USER audio
+sudo gpasswd -a $USER bluetooth
 
-mkdir -p ~/.config/pipewire/pipewire.conf.d
-ln -sf /usr/share/examples/wireplumber/10-wireplumber.conf ~/.config/pipewire/pipewire.conf.d/
-ln -sf /usr/share/examples/pipewire/20-pipewire-pulse.conf ~/.config/pipewire/pipewire.conf.d/
+sudo mkdir -p /etc/alsa/conf.d
+sudo ln -sf /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d/
+sudo ln -sf /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d/
+
+echo "Applying GSettings..."
+apply_gs() {
+    if [ -z "$WAYLAND_DISPLAY" ] && [ -z "$DISPLAY" ]; then
+        # in a TTY, need a temporary DBus session
+        dbus-run-session gsettings set "$1" "$2" "$3"
+    else
+        # in Sway
+        gsettings set "$1" "$2" "$3"
+    fi
+}
+
+apply_gs org.gnome.desktop.interface font-name 'Iosevka 12'
+apply_gs org.gnome.desktop.interface monospace-font-name 'Iosevka Nerd Font 12'
+apply_gs org.gnome.desktop.interface document-font-name 'Iosevka 12'
+apply_gs org.gnome.desktop.wm.preferences titlebar-font 'Iosevka Bold 12'
+apply_gs org.gnome.desktop.interface color-scheme 'prefer-dark'
+apply_gs org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+apply_gs org.gnome.desktop.interface gtk-key-theme 'Emacs'
+apply_gs org.gnome.desktop.interface icon-theme 'Adwaita'
+apply_gs org.gnome.desktop.peripherals.touchpad tap-to-click true
+apply_gs org.gnome.desktop.peripherals.touchpad natural-scroll true
 
 if ! grep -q "MOZ_ENABLE_WAYLAND" ~/.bashrc; then
-    echo 'export XDG_RUNTIME_DIR=/run/user/$(id -u)' >> ~/.bashrc
-    echo 'export MOZ_ENABLE_WAYLAND=1' >> ~/.bashrc
-    echo 'export XDG_CURRENT_DESKTOP=sway' >> ~/.bashrc
+    {
+      echo 'export MOZ_ENABLE_WAYLAND=1'
+      echo 'export XDG_CURRENT_DESKTOP=sway'
+    } >> ~/.bashrc
 fi
 
 echo "Done! Please reboot."
