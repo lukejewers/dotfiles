e9e7027f971d05f0045a0ba3d48730ff1e0f6f5b
luke <luke.jewers@gmail.com>
2026-05-16 22:58:18 +0100

    [emacs] merge early init section into main emacs use-package

--- init.el
+++ init.el
@@ -1,71 +1,50 @@
-;; ==================== ;;
-;; Early Initialization ;;
-;; ==================== ;;
-
-(setq gc-cons-threshold (* 1024 1024 100)
-      gc-cons-percentage 0.6
-      package-enable-at-startup nil)
-
-(defun my-display-startup-time ()
-  (message "Emacs loaded in %.2f seconds with %d garbage collections"
-           (float-time (time-subtract after-init-time before-init-time)) gcs-done))
-(add-hook 'emacs-startup-hook #'my-display-startup-time)
-(add-hook 'emacs-startup-hook (lambda () (setq gc-cons-threshold (* 1024 1024 16))))
-
-(add-to-list 'initial-frame-alist '(fullscreen . maximized))
-(set-face-attribute 'default nil :background "#181818" :foreground "#e4e4ef")
-(set-language-environment "UTF-8")
-
-(setq default-frame-alist
-      `((ns-transparent-titlebar . t)
-        (background-color . "#181818")
-        (font . ,(if (eq system-type 'darwin) "Iosevka 20" "Iosevka 18"))
-        (menu-bar-lines . 0)
-        (tool-bar-lines . 0)
-        (vertical-scroll-bars . nil)))
-
-(add-to-list 'display-buffer-alist
-             '((or . ((derived-mode . occur-mode)
-                      (derived-mode . grep-mode)
-                      (derived-mode . compilation-mode)
-                      (derived-mode . log-view-mode)
-                      (derived-mode . help-mode)))
-               (display-buffer-reuse-mode-window display-buffer-below-selected)
-               (body-function . select-window)))
-
-(setq frame-title-format '("%f")
-      frame-resize-pixelwise t
-      window-resize-pixelwise t
-      frame-inhibit-implied-resize t
-      ns-pop-up-frames nil
-      ns-use-proxy-icon nil
-      inhibit-startup-screen t
-      inhibit-startup-message t
-      ring-bell-function 'ignore
-      custom-safe-themes t
-      use-dialog-box nil
-      use-short-answers t
-      inhibit-compacting-font-caches t
-      mode-line-format nil
-      warning-minimum-level :error
-      warning-suppress-types '((lexical-binding))
-      custom-file (locate-user-emacs-file "custom-vars.el")
-      package-archives '(("melpa"  . "https://melpa.org/packages/")
-                         ("gnu"    . "https://elpa.gnu.org/packages/")
-                         ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
-
-(load custom-file t t)
-(when (eq system-type 'darwin)
-  (setq mac-left-control-modifier 'control
-        mac-option-modifier 'meta
-        mac-command-modifier 'super))
-
-;; ================== ;;
-;; Main Configuration ;;
-;; ================== ;;
-
 (use-package emacs
   :ensure nil
+  :init
+  (setq gc-cons-threshold (* 1024 1024 100)
+        gc-cons-percentage 0.6)
+  (add-to-list 'initial-frame-alist '(fullscreen . maximized))
+  (set-face-attribute 'default nil :background "#181818" :foreground "#e4e4ef")
+  (set-language-environment "UTF-8")
+  (setq default-frame-alist
+        `((ns-transparent-titlebar . t)
+          (background-color . "#181818")
+          (font . ,(if (eq system-type 'darwin) "Iosevka 20" "Iosevka 18"))
+          (menu-bar-lines . 0)
+          (tool-bar-lines . 0)
+          (vertical-scroll-bars . nil)))
+  (add-to-list 'display-buffer-alist
+               '((or . ((derived-mode . occur-mode)
+                        (derived-mode . grep-mode)
+                        (derived-mode . compilation-mode)
+                        (derived-mode . log-view-mode)
+                        (derived-mode . help-mode)))
+                 (display-buffer-reuse-mode-window display-buffer-below-selected)
+                 (body-function . select-window)))
+  (setq frame-title-format '("%f")
+        frame-resize-pixelwise t
+        window-resize-pixelwise t
+        frame-inhibit-implied-resize t
+        inhibit-startup-screen t
+        inhibit-startup-message t
+        ring-bell-function 'ignore
+        use-dialog-box nil
+        use-short-answers t
+        inhibit-compacting-font-caches t
+        mode-line-format nil
+        warning-minimum-level :error
+        warning-suppress-types '((lexical-binding))
+        custom-file (locate-user-emacs-file "custom-vars.el")
+        package-archives '(("melpa"  . "https://melpa.org/packages/")
+                           ("gnu"    . "https://elpa.gnu.org/packages/")
+                           ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
+  (load custom-file t t)
+  (when (eq system-type 'darwin)
+    (setq ns-pop-up-frames nil
+          ns-use-proxy-icon nil
+          mac-left-control-modifier 'control
+          mac-option-modifier 'meta
+          mac-command-modifier 'super))
   :custom
   (auto-save-default nil)
   (c-ts-mode-indent-offset 4)
@@ -99,19 +78,14 @@
   (tab-always-indent 'complete)
   (tab-width 4)
   (use-package-always-ensure t)
-  (use-package-compute-statistics t)
+  (use-package-compute-statistics nil)
   (whitespace-line-column 80)
   (xref-search-program 'ripgrep)
   :config
   (blink-cursor-mode 0)
   (delete-selection-mode 1)
-  (editorconfig-mode 1)
   (electric-pair-mode 1)
-  (global-auto-revert-mode 1)
-  (global-completion-preview-mode 1)
-  (pixel-scroll-precision-mode 1)
   (put 'narrow-to-region 'disabled nil)
-  (savehist-mode 1)
   (show-paren-mode 1)
   (tooltip-mode 0)
   (with-eval-after-load 'c-ts-mode (define-key c-ts-mode-map (kbd "C-c .") nil))
@@ -137,20 +111,26 @@
    ("M-3" . (lambda () (interactive) (insert "#")))
    ("M-o" . other-window))
   :hook
-  (after-save . executable-make-buffer-file-executable-if-script-p)
-  (before-save . whitespace-cleanup)
-  (compilation-filter . ansi-color-compilation-filter)
-  (html-mode . (lambda () (local-unset-key (kbd "M-o"))))
-  (ibuffer-mode . hl-line-mode)
-  (shell-mode . (lambda () (setq-local scroll-margin 1))))
+  ((after-save . executable-make-buffer-file-executable-if-script-p)
+   (before-save . whitespace-cleanup)
+   (compilation-filter . ansi-color-compilation-filter)
+   (emacs-startup . (lambda () (message "Emacs loaded in %.2f seconds with %d garbage collections"
+                               (float-time (time-subtract after-init-time before-init-time)) gcs-done)))
+   (emacs-startup . (lambda () (setq gc-cons-threshold (* 1024 1024 16))))
+   (emacs-startup . editorconfig-mode)
+   (emacs-startup . global-auto-revert-mode)
+   (emacs-startup . global-completion-preview-mode)
+   (emacs-startup . pixel-scroll-precision-mode)
+   (emacs-startup . savehist-mode)
+   (html-mode . (lambda () (local-unset-key (kbd "M-o"))))
+   (ibuffer-mode . hl-line-mode)))
 
 (use-package gruber-darker-theme
   :hook (after-init . (lambda () (load-theme 'gruber-darker t))))
 
 (use-package ido
   :ensure nil
-  :demand t
-  :config (ido-mode 1)
+  :hook (after-init . ido-mode)
   :custom
   (ido-auto-merge-work-directories-length -1)
   (ido-max-prospects 10)
@@ -160,7 +140,8 @@
 
 (use-package ido-completing-read+
   :after ido
-  :config (ido-ubiquitous-mode 1))
+  :config
+  (ido-ubiquitous-mode 1))
 
 (use-package amx
   :hook (after-init . amx-mode))
