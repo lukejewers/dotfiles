62f92eaed294e8afdff162f8807df2b813582f6b
luke <luke.jewers@gmail.com>
2026-09-05 12:57:59 +0100

    [emacs] rm eglot

--- init.el
+++ init.el
@@ -321,34 +321,6 @@
   (magit-git-executable
    (if (eq system-type 'darwin) "/opt/homebrew/bin/git" "git")))
 
-(use-package eglot
-  :ensure nil
-  :hook
-  (prog-mode . eglot-ensure)
-  (eglot-managed-mode . (lambda () (flymake-mode -1)))
-  :custom
-  (eglot-autoshutdown t)
-  (eglot-code-action-indications nil)
-  (eglot-events-buffer-config '(:size 0 :format short))
-  (eglot-report-progress nil)
-  (eglot-stay-out-of '(flymake eldoc))
-  (eglot-sync-connect nil)
-  (eglot-ignored-server-capabilities
-   '(:codeActionProvider
-     :codeLensProvider
-     :declarationProvider
-     :diagnosticProvider
-     :documentFormattingProvider
-     :documentHighlightProvider
-     :documentSymbolProvider
-     :hoverProvider
-     :implementationProvider
-     :inlayHintProvider
-     :renameProvider
-     :signatureHelpProvider
-     :semanticTokensProvider
-     :typeDefinitionProvider)))
-
 (use-package gptel
   :defer t
   :ensure t
